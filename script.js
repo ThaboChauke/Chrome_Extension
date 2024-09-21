7cd959dc344e20a37f65b74cf27e90d1b211306e
ThaboChauke <chaukethabo16@gmail.com>
2024-09-22 00:28:09 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const inputEl = document.getElementById("input-el")
 const saveBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-let myLeads = []
+let myLeads = [1,2,3,4]
 
 saveBtn.addEventListener ("click", function() {
     myLeads.push(inputEl.value)
 })
 
+myLeads.forEach(lead => {
+    ulEl.textContent += lead
+    console.log(lead)
+})
