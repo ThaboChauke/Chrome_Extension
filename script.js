309430eeaabccd72ea011bd50a8e243ab425a5f1
Thabo Chauke <chaukethabo16@gmail.com>
2024-09-22 16:46:30 +0200

    save to local storage

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const ulEl = document.getElementById("ul-el")
 
 let myLeads = []
 
+// localStorage.setItem("myLeads", "www.leadexample.com")
+// console.log(localStorage.getItem("myLeads"))
+// localStorage.clear()
+
 saveBtn.addEventListener ("click", function() {
     myLeads.push(inputEl.value)
     renderLeads()
     inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    console.log(localStorage.getItem("myLeads"))
 })
 
 
